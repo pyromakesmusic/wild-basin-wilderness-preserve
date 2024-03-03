a44e7a111e7707d526ee22bdc408a2db12a59347
pyromakesmusic <pyromakesmusic@gmail.com>
2024-03-03 16:34:32 -0600

    adding aquifer and precipitation data

--- analysis_scratch.py
+++ analysis_scratch.py
@@ -9,7 +9,9 @@ import matplotlib.pyplot as plt
 import geopandas as gpd
 
 # Reading in Shapefiles
-terrain_df = gpd.read_file(r"data\USGS_HUC_8_Shapefile\USGS_HUC_8_Subbasin.shp")
+maj_aquifer_df = gpd.read_file(r"data\major_aquifers\NEW_major_aquifers_dd.shp")
+subbasin_df = gpd.read_file(r"data\USGS_HUC_8_Shapefile\USGS_HUC_8_Subbasin.shp") # Subbasin file
+precip_df = gpd.read_file(r"data\Precipitation_Shapefile\TX_Precip_1981_2010_NRCS.shp")
 
-terrain_df.plot()
+maj_aquifer_df.plot()
 plt.show()

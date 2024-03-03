60959ce6b2b853068517858172d39fe4c5ec2137
pyromakesmusic <pyromakesmusic@gmail.com>
2024-03-03 16:05:12 -0600

    adding some commentary and preparing to load in data

--- analysis_scratch.py
+++ analysis_scratch.py
@@ -3,4 +3,7 @@ By: Tayo Castro
 Description: Some early exploration of geographic data related to the Wild Basin Wilderness Preserve.
 """
 
+# Library Imports
 import geopandas as gpd
+
+# Reading in Shapefiles

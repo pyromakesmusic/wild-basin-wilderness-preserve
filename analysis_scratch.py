a7b77b87e52d30ede7ab997ac77afe719c84ede0
pyromakesmusic <pyromakesmusic@gmail.com>
2024-03-03 16:09:18 -0600

    got texas terrain read in

--- analysis_scratch.py
+++ analysis_scratch.py
@@ -4,6 +4,12 @@ Description: Some early exploration of geographic data related to the Wild Basin
 """
 
 # Library Imports
+import matplotlib as mpl
+import matplotlib.pyplot as plt
 import geopandas as gpd
 
 # Reading in Shapefiles
+terrain_df = gpd.read_file(r"data\USGS_HUC_8_Shapefile\USGS_HUC_8_Subbasin.shp")
+
+terrain_df.plot()
+plt.show()

37303bc6c3b5d66640c8c9acdb218c2392dae5c1
pyromakesmusic <pyromakesmusic@gmail.com>
2024-03-03 16:37:14 -0600

    more aquifer data

--- analysis_scratch.py
+++ analysis_scratch.py
@@ -9,9 +9,10 @@ import matplotlib.pyplot as plt
 import geopandas as gpd
 
 # Reading in Shapefiles
-maj_aquifer_df = gpd.read_file(r"data\major_aquifers\NEW_major_aquifers_dd.shp")
-subbasin_df = gpd.read_file(r"data\USGS_HUC_8_Shapefile\USGS_HUC_8_Subbasin.shp") # Subbasin file
-precip_df = gpd.read_file(r"data\Precipitation_Shapefile\TX_Precip_1981_2010_NRCS.shp")
+maj_aquifer_df = gpd.read_file(r"data\major_aquifers\NEW_major_aquifers_dd.shp")  # Major aquifer file
+min_aquifer_df = gpd.read_file(r"data\minor_aquifers\Minor_Aquifers.shp")  # Minor aquifer file
+subbasin_df = gpd.read_file(r"data\USGS_HUC_8_Shapefile\USGS_HUC_8_Subbasin.shp")  # Subbasin file
+precip_df = gpd.read_file(r"data\Precipitation_Shapefile\TX_Precip_1981_2010_NRCS.shp")  # Precipitation file
 
-maj_aquifer_df.plot()
+min_aquifer_df.plot()
 plt.show()

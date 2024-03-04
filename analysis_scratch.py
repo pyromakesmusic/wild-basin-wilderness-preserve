8b4510cb73a53adaf344b619b2d83f569539f791
pyromakesmusic <pyromakesmusic@gmail.com>
2024-03-03 20:29:56 -0600

    printing coordinate reference systems

--- analysis_scratch.py
+++ analysis_scratch.py
@@ -8,19 +8,30 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 import geopandas as gpd
 
-# Reading in Shapefiles
-# maj_aquifer_df = gpd.read_file(r"data\major_aquifers\NEW_major_aquifers_dd.shp")  # Major aquifer file
-# min_aquifer_df = gpd.read_file(r"data\minor_aquifers\Minor_Aquifers.shp")  # Minor aquifer file
-# subbasin_df = gpd.read_file(r"data\USGS_HUC_8_Shapefile\USGS_HUC_8_Subbasin.shp")  # Subbasin file
-# precip_df = gpd.read_file(r"data\Precipitation_Shapefile\TX_Precip_1981_2010_NRCS.shp")  # Precipitation file
+"""
+Reading in Shapefiles
+"""
+
+maj_aquifer_df = gpd.read_file(r"data\major_aquifers\NEW_major_aquifers_dd.shp")  # Major aquifer file
+min_aquifer_df = gpd.read_file(r"data\minor_aquifers\Minor_Aquifers.shp")  # Minor aquifer file
+subbasin_df = gpd.read_file(r"data\USGS_HUC_8_Shapefile\USGS_HUC_8_Subbasin.shp")  # Subbasin file
+precip_df = gpd.read_file(r"data\Precipitation_Shapefile\TX_Precip_1981_2010_NRCS.shp")  # Precipitation file
 rivers_df = gpd.read_file(r"data\Major_Rivers_dd83\MajorRivers_dd83.shp")  # Rivers file
 river_basins_df = gpd.read_file(r"data\Major_River_Basins_Shapefile\TWDB_MRBs_2014.shp")  # River basins file
-# reservoirs_df = gpd.read_file(r"data\existing_reservoirs\TWDB_SWP2012_Major_Reservoirs.shp")  # Reservoirs file
-# groundwater_df = gpd.read_file(r"data\TWDB_Groundwater\TWDB_Groundwater.shp")  # Groundwater file
-# floodplan_df = gpd.read_file(r"data\Regional_Flood_Planning_Groups\Regional_Flood_Planning_Groups.shp")  # Flood planning file
+reservoirs_df = gpd.read_file(r"data\existing_reservoirs\TWDB_SWP2012_Major_Reservoirs.shp")  # Reservoirs file
+groundwater_df = gpd.read_file(r"data\TWDB_Groundwater\TWDB_Groundwater.shp")  # Groundwater file
+floodplan_df = gpd.read_file(r"data\Regional_Flood_Planning_Groups\Regional_Flood_Planning_Groups.shp")  # Flood planning file
 
+df_list = [maj_aquifer_df, min_aquifer_df, subbasin_df, precip_df, rivers_df, river_basins_df, reservoirs_df, groundwater_df,
+           floodplan_df]
 
-rivers_df.plot(alpha=0.2, color="blue")
-river_basins_df.plot(alpha=0.2, color="cyan")
+for x in df_list:
+    print(str(df_list), str(df_list.crs))
+"""
+Plotting
+"""
+
+# rivers_df.plot(alpha=0.2, color="blue")
+# river_basins_df.plot(alpha=0.2, color="cyan")
 # plt.legend()
-plt.show()
+# plt.show()

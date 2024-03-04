e670ef2314220196e1314fdb41093918f9d8547d
pyromakesmusic <pyromakesmusic@gmail.com>
2024-03-03 19:50:12 -0600

    turning off all but one

--- analysis_scratch.py
+++ analysis_scratch.py
@@ -13,15 +13,14 @@ import geopandas as gpd
 # min_aquifer_df = gpd.read_file(r"data\minor_aquifers\Minor_Aquifers.shp")  # Minor aquifer file
 # subbasin_df = gpd.read_file(r"data\USGS_HUC_8_Shapefile\USGS_HUC_8_Subbasin.shp")  # Subbasin file
 # precip_df = gpd.read_file(r"data\Precipitation_Shapefile\TX_Precip_1981_2010_NRCS.shp")  # Precipitation file
-# rivers_df = gpd.read_file(r"data\Major_Rivers_dd83\MajorRivers_dd83.shp")  # Rivers file
-# river_basins_df = gpd.read_file(r"data\Major_River_Basins_Shapefile\TWDB_MRBs_2014.shp")  # River basins file
+rivers_df = gpd.read_file(r"data\Major_Rivers_dd83\MajorRivers_dd83.shp")  # Rivers file
+river_basins_df = gpd.read_file(r"data\Major_River_Basins_Shapefile\TWDB_MRBs_2014.shp")  # River basins file
 # reservoirs_df = gpd.read_file(r"data\existing_reservoirs\TWDB_SWP2012_Major_Reservoirs.shp")  # Reservoirs file
 # groundwater_df = gpd.read_file(r"data\TWDB_Groundwater\TWDB_Groundwater.shp")  # Groundwater file
 # floodplan_df = gpd.read_file(r"data\Regional_Flood_Planning_Groups\Regional_Flood_Planning_Groups.shp")  # Flood planning file
-hillshade_df = gpd.read_file(r"data\hillshade\tx_terrain_hillshade_tsms.tif")
 
 
-
-hillshade_df.plot()
+rivers_df.plot(alpha=0.2, color="blue")
+river_basins_df.plot(alpha=0.2, color="cyan")
 # plt.legend()
 plt.show()

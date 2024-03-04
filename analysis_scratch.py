51fc1599a96c9598b19338a2874ba75673ec6a05
pyromakesmusic <pyromakesmusic@gmail.com>
2024-03-03 18:51:08 -0600

    most of shapefiles loaded

--- analysis_scratch.py
+++ analysis_scratch.py
@@ -16,8 +16,12 @@ precip_df = gpd.read_file(r"data\Precipitation_Shapefile\TX_Precip_1981_2010_NRC
 rivers_df = gpd.read_file(r"data\Major_Rivers_dd83\MajorRivers_dd83.shp")  # Rivers file
 river_basins_df = gpd.read_file(r"data\Major_River_Basins_Shapefile\TWDB_MRBs_2014.shp")  # River basins file
 reservoirs_df = gpd.read_file(r"data\existing_reservoirs\TWDB_SWP2012_Major_Reservoirs.shp")  # Reservoirs file
+groundwater_df = gpd.read_file(r"data\TWDB_Groundwater\TWDB_Groundwater.shp")  # Groundwater file
+floodplan_df = gpd.read_file(r"data\Regional_Flood_Planning_Groups\Regional_Flood_Planning_Groups.shp")  # Flood planning file
+
 
 
 
 min_aquifer_df.plot()
+# plt.legend()
 plt.show()
